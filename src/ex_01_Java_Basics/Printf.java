90130c55f030b69b3a00eb1e89186c35ec97c35c
AkshathaChidgal <cakshathachidgal@gmail.com>
2025-03-11 05:13:47 -0700

    Print the table of 9 by using the printf command

---
+++ src/ex_01_Java_Basics/Printf.java
@@ -0,0 +1,27 @@
+package ex_01_Java_Basics;
+
+//int a = 10;
+//System.out.printf("You variable is %d",a);
+
+//int table = 9
+
+//9×1 = 9
+//9×2 = 18
+
+
+public class Printf {
+    public static void main(String[] args) {
+        int table=9;
+        System.out.printf("%dx1=%d%n",table,table*1);
+        System.out.printf("%dx2=%d%n",table,table*2);
+        System.out.printf("%dx3=%d%n",table,table*3);
+        System.out.printf("%dx4=%d%n",table,table*4);
+        System.out.printf("%dx5=%d%n",table,table*5);
+        System.out.printf("%dx6=%d%n",table,table*6);
+        System.out.printf("%dx7=%d%n",table,table*7);
+        System.out.printf("%dx8=%d%n",table,table*8);
+        System.out.printf("%dx9=%d%n",table,table*9);
+        System.out.printf("%dx10=%d%n",table,table*10);
+
+    }
+}
